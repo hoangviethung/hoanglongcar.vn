dea08a12cca3c0472f32798ce5c996e229b40f2f
hoangviethung <hoangviet.hung.009@gmail.com>
2021-01-27 00:49:09 +0700

    update index

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,9 +3,25 @@ import { HTTP } from './services/http';
 HTTP.get('https://api.github.com/users?per_page=5').subscribe((res) => {});
 
 const initHeroBanner = () => {
-	let heroBanner = new Swiper('.hero-banner .swiper-container');
+	let slider = new Swiper('.hero-banner .swiper-container');
+};
+
+const initSliderCategoryProduct = () => {
+	let sliderThumb = new Swiper(
+		'.index-categories-product .slider-thumb .swiper-container',
+		{
+			slidesPerView: 4,
+		},
+	);
+	let sliderActive = new Swiper(
+		'.index-categories-product .slider-active .swiper-container',
+		{
+			effect: 'fade',
+		},
+	);
 };
 
 document.addEventListener('DOMContentLoaded', () => {
 	initHeroBanner();
+	initSliderCategoryProduct();
 });
