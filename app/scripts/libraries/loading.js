fc97168d49ddb4fc9510a92e832a08357490fea6
hoangviethung <hoangviet.hung.009@gmail.com>
2021-02-13 23:55:24 +0700

    done

--- app/scripts/libraries/loading.js
+++ app/scripts/libraries/loading.js
@@ -1,48 +1,43 @@
-export const Loading = () => {
+module.exports = (cb) => {
 	let loading = document.querySelector('#loading-container');
+	let images = document.images;
+	let imagesLength = images.length;
+	let counter = 0;
+
+	function turnOffLoadingScreen() {
+		loading.style.opacity = '0';
+		setTimeout(function () {
+			loading.parentNode.removeChild(loading);
+			document.querySelector('body').classList.add('show-page');
+			if (typeof cb == 'function') cb();
+		}, 500);
+	}
+
+	function progressing() {
+		let progressBar = loading.querySelector('#progress-bar');
+		let progressPercentage = loading.querySelector('#progress-percentage');
+		counter += 1;
+		let n = Math.round((100 / imagesLength) * counter);
+		if (progressBar) {
+			progressBar.style.width = `${n}%`;
+		}
+		if (progressPercentage) {
+			progressPercentage.innerHTML = `${n}`;
+		}
+		if (counter === imagesLength) {
+			return turnOffLoadingScreen();
+		}
+	}
 	if (loading) {
-		return new Promise((resolve, reject) => {
-			let images = document.images;
-			let imagesLength = images.length;
-			let counter = 0;
-
-			function turnOffLoadingScreen() {
-				loading.style.opacity = '0';
-				setTimeout(function () {
-					loading.parentNode.removeChild(loading);
-					document.querySelector('body').classList.add('show-page');
-					resolve();
-				}, 500);
+		if (imagesLength === 0) {
+			return turnOffLoadingScreen();
+		} else {
+			for (let i = 0; i < imagesLength; i++) {
+				let img = new Image();
+				img.onload = progressing;
+				img.onerror = progressing;
+				img.src = images[i].src;
 			}
-
-			function progressing() {
-				counter += 1;
-				let progressBar = loading.querySelector('#progress-bar');
-				let progressPercentage = loading.querySelector(
-					'#progress-percentage',
-				);
-				let n = Math.round((100 / imagesLength) * counter);
-
-				if (progressBar) {
-					progressBar.style.width = `${n}%`;
-				}
-				if (progressPercentage) {
-					progressPercentage.innerHTML = `${n}`;
-				}
-				if (counter === imagesLength) {
-					return turnOffLoadingScreen();
-				}
-			}
-			if (imagesLength === 0) {
-				return turnOffLoadingScreen();
-			} else {
-				for (let i = 0; i < imagesLength; i++) {
-					let img = new Image();
-					img.onload = progressing;
-					img.onerror = progressing;
-					img.src = images[i].src;
-				}
-			}
-		});
+		}
 	}
 };
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 import Tab from './libraries/tabs';
-
+import Loading from './libraries/loading';
 const fixedHeaderWhenScroll = () => {
 	const header = document.querySelector('header');
 	if (window.pageYOffset > header.offsetHeight) {
@@ -401,6 +401,7 @@ const deleteRowTableCart = () => {
 };
 
 window.addEventListener('load', (e) => {
+	Loading();
 	initializeMasonryJs();
 	initializeWowJs();
 	initializeFancyAppJs();
